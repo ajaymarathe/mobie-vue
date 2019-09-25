129d61a5ee16f75814b77c2c2a855f682c5280e2
Ajay Marathe <ajay.marathe@xebecindia.com>
2019-09-25 12:59:00 +0530

    added docs folder

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,35 +1,32 @@
 <template>
-    <div>
-        <!-- navbar  -->
-        <nav class="nav mobie-nav d-flex align-items-center">
-          <a href="home.html">
-              <i @click="back()" class="material-icons ml-3">
-                  arrow_back
-              </i>
-          </a>
-          <a class="nav-link" href="#">{{title}}</a>
-        </nav>
-        <!-- end navbar -->
-    </div>
+  <div>
+    <!-- navbar  -->
+    <nav class="nav mobie-nav d-flex align-items-center">
+      <a href="home.html">
+        <i @click="back()" class="material-icons ml-3">arrow_back</i>
+      </a>
+      <a class="nav-link">{{ title }}</a>
+    </nav>
+    <!-- end navbar -->
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "navbar",
-        props: ['title'],
-        data:function(){
-            return {
-                message: "navbar"
-            }
-        },
-        methods:{
-            back(){
-                router.go(-1)
-            }
-        }
+export default {
+  name: "navbar",
+  props: ["title"],
+  data: function() {
+    return {
+      message: "navbar"
+    };
+  },
+  methods: {
+    back() {
+      router.go(-1);
     }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
